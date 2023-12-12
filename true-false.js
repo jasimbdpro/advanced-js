14afa129f78ca3b3c3f3fdb697b7e4aaf078ad1f
jasimbdpro <jasimbdpro@gmail.com>
2023-12-12 23:06:33 +0600

    new

--- true-false.js
+++ true-false.js
@@ -1,7 +1,10 @@
-let age = NaN;
+let age = {};
 if (age) {
     console.log('condition is true')
 }
 else {
     console.log('condition is false')
 }
+
+// falsy Values: 0, false, '', "", undefined, null, NaN, -0 etc
+//truthy Value; '0', true, ' ', " ", [], {}, 'false', 
